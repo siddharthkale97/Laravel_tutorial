2c14ac3edb2487f12e0096af168bbc7feb241b69
Siddharth Kale <siddharthkale97@gmail.com>
2018-05-01 18:15:26 +0530

    Models Made

--- app/User.php
+++ app/User.php
@@ -15,7 +15,14 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'city',
+        'roll_id',
     ];
 
     /**
